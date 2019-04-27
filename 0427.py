82f9cd90ad65285f818bdc9d06b3fb1a9d89b67b
etoos <49575896+etoos@users.noreply.github.com>
2019-04-27 12:43:26 +0900

    Add files via upload

---
+++ 0427.py
@@ -0,0 +1,9 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("오늘도 멋진 하루였어")
+print("나는 하고싶은 일이 정말 많아")
+print("오늘 참 열심히 공부했어 푹 잘수 있겠다")
+print("넌 내게 소중한 사람이야")
+print("난 너무 잘생겼어")
+print("난 성실하니까 모두가 인정해주잖아")
+print("화이팅~")
